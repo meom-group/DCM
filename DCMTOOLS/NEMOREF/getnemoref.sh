f103e0081f7472fafb02c679b98c075011398c72
jmm <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2018-12-07 00:02:00 +0100

    give explanations about XIOS in the getnemoref

--- DCMTOOLS/NEMOREF/getnemoref.sh
+++ DCMTOOLS/NEMOREF/getnemoref.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+# note that you also need xios2 for compiling the code
+#  This NEMO revision was compiled and ran successfully with
+# xios rev 1587 of http://forge.ipsl.jussieu.fr/ioserver/svn/XIOS/dev/dev_olga
+# to be compiled out of the DCM structure.
+#  NEMO fcm files should indicate the xios root directory
 #svn co http://forge.ipsl.jussieu.fr/nemo/svn/NEMO/trunk -r 10089 NEMO4
 svn co http://forge.ipsl.jussieu.fr/nemo/svn/NEMO/trunk -r 10374 NEMO4
