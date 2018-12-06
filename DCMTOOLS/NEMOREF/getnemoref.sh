a1fd53cf2e47430442145a4ece1b867df33698e5
Jean-Marc Molines <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2018-12-06 12:50:28 +0100

    update getnemoref.sh to actual trunk revision

--- DCMTOOLS/NEMOREF/getnemoref.sh
+++ DCMTOOLS/NEMOREF/getnemoref.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-svn co http://forge.ipsl.jussieu.fr/nemo/svn/NEMO/trunk -r 10089 NEMO4
+#svn co http://forge.ipsl.jussieu.fr/nemo/svn/NEMO/trunk -r 10089 NEMO4
+svn co http://forge.ipsl.jussieu.fr/nemo/svn/NEMO/trunk -r 10374 NEMO4
