c00ac7de971ad332fe8200025bb4e6c6f35c67c3
jmm <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2021-03-16 21:07:24 +0100

    add SBATCH directive to avoid multithreading in CONFCASE_jean-zay.sh
       * otherwise, the number of core fper node is seen as 80 instead of 40
       * this behaviour produces error in runcode_mpmd_dp

--- RUNTOOLS/templates/CONFCASE_jean-zay.sh
+++ RUNTOOLS/templates/CONFCASE_jean-zay.sh
@@ -5,7 +5,8 @@
 #SBATCH -J nemo_jean-zay
 #SBATCH -e nemo_jean-zay.e%j
 #SBATCH -o nemo_jean-zay.o%j
-#SBATCH -A fqx@cpu
+#SBATCH -A cli@cpu
+#SBATCH --hint=nomultithread
 #SBATCH --time=1:00:00
 #SBATCH --exclusive
 
@@ -19,6 +20,12 @@ CASE=<CASE>
 CONFCASE=${CONFIG}-${CASE}
 CTL_DIR=$PDIR/RUN_${CONFIG}/${CONFCASE}/CTL
 
+##################################################################
+#  WARNING: On Jean-Zay, It seems that having NEMO and XIOS_SERVER
+#        running on the same node, lead to freezing in iom_init
+#        A workaround is to put all the xios on a separate node 
+#        using NB_NCORE_DP /= 0 
+#################################################################
 # Following numbers must be consistant with the header of this job
 export NB_NPROC=39     # number of cores used for NEMO
 export NB_NPROC_IOS=1  # number of cores used for xios (number of xios_server.exe)
@@ -29,7 +36,7 @@ export MERGE=0         # 1 = on the fly rebuild, 0 = dedicated job
 export NB_NPROC_MER=15 # number of cores used for rebuild on the fly  (1/node is a good choice)
 export NB_NNODE_MER=1  # number of nodes used for rebuild in dedicated job (MERGE=0). One instance of rebuild per node will be used.
 export WALL_CLK_MER=3:00:00   # wall clock time for batch rebuild
-export ACCOUNT=fqx@cpu # account to be used
+export ACCOUNT=cli@cpu # account to be used
 
 date
 #
